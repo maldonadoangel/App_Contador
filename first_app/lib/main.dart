26068a9f4f5280d7b0302f9309d33654b33176a3
Angel Morales <maldonadoangel7069@gmail.com>
2022-01-16 00:47:33 -0600

    Mi primera aplicacion en Flutter, se termino el contador de personas

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:first_app/screens/counter_screen.dart';
 import 'package:flutter/material.dart';
 
+//Usualmente casi siempre, creamos nuestra funcion principal y llamamos nuestro primer widget para que se renderice
+//nuestro primer widget que es MyApp, usualmente posee dentro un MaterialApp
 void main() {
   runApp(const MyApp());
 }
 
+//El statelesswidget, siempre necesita un widget build que se encuentre sobre escrito (@override)
+// el widget build siempre necesita regresar otro widget, este tipo de widget se maneja siempre de manera sincrona
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
--- first_app/lib/screens/counter_screen.dart
+++ first_app/lib/screens/counter_screen.dart
@@ -11,15 +11,34 @@ class CounterScreen extends StatefulWidget {
 class _CounterScreenState extends State<CounterScreen> {
   int counter = 40;
   //variable o propiedad
+
+  void increase() {
+    counter++;
+    setState(() {});
+  }
+
+  void decrease() {
+    counter--;
+    setState(() {});
+  }
+
+  void zero() {
+    counter = 0;
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     //Creamos una variable
     const fontSize30 = TextStyle(fontSize: 30);
 
+    //El scaffold es el lienzo blanco de nuestra app, le colocamos los widgets que deseemos
+    //no siempre tendremos un appbar, no siempre tendremos un un floatinActionButton, pero casi el 99% tendremos el body
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.red,
         title: const Text('CounterScreen'),
-        elevation: 0,
+        centerTitle: true,
       ),
       body: Center(
         child: Column(
@@ -37,14 +56,54 @@ class _CounterScreenState extends State<CounterScreen> {
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: () {
-          print('Presionaron el boton');
-          counter++;
-          setState(() {});
-        },
-      ),
+      //Pasamos parametros a nuestra funcion widget floatingActionButton, funcion que iniciamos en nuestro customFloatingAction
+
+      floatingActionButton: CustomFloatingActions(
+          increaseFn: increase, decreaseFn: decrease, zeroFn: zero),
+    );
+  }
+}
+
+class CustomFloatingActions extends StatelessWidget {
+  final Function increaseFn;
+  final Function decreaseFn;
+  final Function zeroFn;
+  const CustomFloatingActions(
+      {Key? key,
+      required this.increaseFn,
+      required this.decreaseFn,
+      required this.zeroFn})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        FloatingActionButton(
+          backgroundColor: Colors.red,
+          child: const Icon(Icons.add),
+          onPressed: () => increaseFn(),
+        ),
+        const SizedBox(
+          width: 20,
+        ),
+        //Ejemplo sin funcion flecha, solo una funcion normal
+        FloatingActionButton(
+            backgroundColor: Colors.red,
+            child: const Icon(Icons.exposure_zero),
+            onPressed: () {
+              zeroFn();
+            }),
+        const SizedBox(
+          width: 20,
+        ),
+        FloatingActionButton(
+          backgroundColor: Colors.red,
+          child: const Icon(Icons.exposure_minus_1_outlined),
+          onPressed: () => decreaseFn(),
+        ),
+      ],
     );
   }
 }
